2b29d949551823e3c7d8f4f91c97a9456776c5d7
Your Name <you@example.com>
2020-01-02 20:40:03 -0200

    Exclusao de fabricante

--- src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
+++ src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
@@ -1,11 +1,15 @@
 package com.algaworks.curso.jpa2.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceException;
 
 import com.algaworks.curso.jpa2.modelo.Fabricante;
+import com.algaworks.curso.jpa2.service.NegocioException;
+import com.algaworks.curso.jpa2.util.jpa.Transactional;
 
 public class FabricanteDAO implements Serializable{
 	
@@ -14,5 +18,23 @@ public class FabricanteDAO implements Serializable{
 	
 	public void salvar(Fabricante fabricante) {
 		em.persist(fabricante);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Fabricante> buscarTodos() {
+		// TODO Auto-generated method stub
+		return em.createQuery("from Fabricante").getResultList();
+	}
+	
+	@Transactional
+	public void excluir(Fabricante fabricanteSelecionado) throws NegocioException{
+		try {
+			Fabricante fabricante = em.find(Fabricante.class, fabricanteSelecionado.getCodigo());
+			em.remove(fabricante);
+			em.flush();
+			
+		} catch (PersistenceException e) {
+			// TODO: handle exception
+		}
 	}
 }
