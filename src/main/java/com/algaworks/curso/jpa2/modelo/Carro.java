295146a12dd6d164e79cd579a3d327c7beb02707
Your Name <you@example.com>
2020-01-05 19:07:30 -0200

    Relacionameto um para muitos e mappedBy para indicar a propriedade mapeada

--- src/main/java/com/algaworks/curso/jpa2/modelo/Carro.java
+++ src/main/java/com/algaworks/curso/jpa2/modelo/Carro.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Carro {
@@ -21,61 +22,78 @@ public class Carro {
 	private BigDecimal valorDiaria;
 	private ModeloCarro modelo;
 	private List<Acessorio> acessorios;
-	
+	private List<Aluguel> alugueis;
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(Long codigo) {
 		this.codigo = codigo;
 	}
-	
+
 	public String getPlaca() {
 		return placa;
 	}
+
 	public void setPlaca(String placa) {
 		this.placa = placa;
 	}
-	
+
 	public String getCor() {
 		return cor;
 	}
+
 	public void setCor(String cor) {
 		this.cor = cor;
 	}
-	
+
 	public String getChassi() {
 		return chassi;
 	}
+
 	public void setChassi(String chassi) {
 		this.chassi = chassi;
 	}
-	
+
 	public BigDecimal getValorDiaria() {
 		return valorDiaria;
 	}
+
 	public void setValorDiaria(BigDecimal valorDiaria) {
 		this.valorDiaria = valorDiaria;
 	}
-	
+
 	@ManyToOne
-	@JoinColumn(name="codigo_modelo")
+	@JoinColumn(name = "codigo_modelo")
 	public ModeloCarro getModelo() {
 		return modelo;
 	}
+
 	public void setModelo(ModeloCarro modelo) {
 		this.modelo = modelo;
 	}
-	
+
 	@ManyToMany
 	public List<Acessorio> getAcessorios() {
 		return acessorios;
 	}
+
 	public void setAcessorios(List<Acessorio> acessorios) {
 		this.acessorios = acessorios;
 	}
 	
+	@OneToMany(mappedBy="carro")
+	public List<Aluguel> getAlugueis() {
+		return alugueis;
+	}
+
+	public void setAlugueis(List<Aluguel> alugueis) {
+		this.alugueis = alugueis;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -83,7 +101,7 @@ public class Carro {
 		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
