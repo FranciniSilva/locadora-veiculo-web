605c06848cc643fff3a3ff3e7c39bf6e7244cbc8
Your Name <you@example.com>
2020-01-04 22:30:39 -0200

    Inserindo joinColumn para mudar nome do mapeamento da chave estrangeira na tabela modelocarro

--- src/main/java/com/algaworks/curso/jpa2/modelo/ModeloCarro.java
+++ src/main/java/com/algaworks/curso/jpa2/modelo/ModeloCarro.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
@@ -15,6 +16,7 @@ public class ModeloCarro {
 	private String descricao;
 	
 	@ManyToOne
+	@JoinColumn(name="codigo_fabricante")
 	private Fabricante fabricante;
 
 	public ModeloCarro() {
