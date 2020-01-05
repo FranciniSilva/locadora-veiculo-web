7b99d24eaf8d64344cb229a01b0370c6fd0ac3f8
Your Name <you@example.com>
2020-01-05 14:55:32 -0200

    mudando nome da tabela

--- src/main/java/com/algaworks/curso/jpa2/modelo/Aluguel.java
+++ src/main/java/com/algaworks/curso/jpa2/modelo/Aluguel.java
@@ -10,8 +10,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="apolice_seguro")
 public class Aluguel {
 
 	private Long codigo;
